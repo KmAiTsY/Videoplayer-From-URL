948de6705bb9c3b5c91666e803c6873ffe1b1785
KmAiTsY <mishagandabura@gmail.com>
2022-01-08 18:46:34 +0200

    Init commit

---
+++ Assets/Scripts/OnlineVideoLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class OnlineVideoLoader : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public InputField inputField;
+    public GameObject resume;
+    public GameObject pause;
+
+    IEnumerator TestCoroutine()
+    {
+        videoPlayer.Prepare();
+        while (!videoPlayer.isPrepared)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        videoPlayer.Play();
+    }
+    public void SubmitAndStart()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        resume.SetActive(false);
+        pause.SetActive(true);
+        videoPlayer.playOnAwake = true;
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.Direct;
+        string videoUrl = inputField.text;
+        videoPlayer.url = videoUrl;
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        videoPlayer.controlledAudioTrackCount = 1;
+        videoPlayer.EnableAudioTrack(0, true);
+        StartCoroutine(TestCoroutine());
+    }
+}
---
+++ Assets/Scripts/VideoControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class VideoControl : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public GameObject resume;
+    public GameObject pause;
+    public void Resume()
+    {
+        videoPlayer.Play();
+        resume.SetActive(false);
+        pause.SetActive(true);
+    }
+    public void Pause()
+    {
+        videoPlayer.Pause();
+        pause.SetActive(false);
+        resume.SetActive(true);
+    }
+}
